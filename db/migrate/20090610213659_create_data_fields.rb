39badb07b7cfe29fdb279ac3acde8616bac08e77
Kwabena Antwi-Boasiako <hikwabena@gmail.com>
2009-06-10 19:37:46 -0400

    Added migration file for data_field stuff, more to come

---
+++ db/migrate/20090610213659_create_data_fields.rb
@@ -0,0 +1,15 @@
+class CreateDataFields < ActiveRecord::Migration
+  def self.up
+    create_table :data_fields do |t|
+      t.string :label
+      t.string :value
+      t.string :type
+      t.integer :data_type_id
+      t.timestamps
+    end
+  end
+  
+  def self.down
+    drop_table :data_fields
+  end
+end
