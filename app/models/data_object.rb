8f020167bae15d81d6abd7eed955b85b0b478da0
Kwabena Antwi-Boasiako <hikwabena@gmail.com>
2009-06-12 14:09:31 -0400

    Made changes to data_type model

--- app/models/data_object.rb
+++ app/models/data_object.rb
@@ -2,4 +2,6 @@ class DataObject < ActiveRecord::Base
   belongs_to :data_type
   has_many :data_entries
   has_and_belongs_to_many :locations
+  
+  validates_uniqueness_of :name
 end
--- app/models/data_type.rb
+++ app/models/data_type.rb
@@ -2,4 +2,6 @@ class DataType < ActiveRecord::Base
   belongs_to :department
   has_many :data_objects
   has_many :data_fields
+  
+  validates_uniqueness_of :name
 end
