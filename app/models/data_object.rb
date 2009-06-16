d921f08f0ffcc8b555a4e3e0d2beede4bcd1405c
Ben Somers <somers.ben@gmail.com>
2009-06-16 12:09:06 -0400

    yesterday's data changes - mostly view-level stuff

--- app/models/data_object.rb
+++ app/models/data_object.rb
@@ -1,7 +1,8 @@
 class DataObject < ActiveRecord::Base
   belongs_to :data_type
-  has_many :data_entries
+  has_many :data_entries, :dependent => :destroy
   has_and_belongs_to_many :locations
   
   validates_uniqueness_of :name
+  validates_presence_of :data_type_id
 end
--- app/models/data_type.rb
+++ app/models/data_type.rb
@@ -1,8 +1,8 @@
 class DataType < ActiveRecord::Base
   belongs_to :department
-  has_many :data_objects
-  has_many :data_fields
+  has_many :data_objects, :dependent => :destroy
+  has_many :data_fields, :dependent => :destroy
   
   validates_uniqueness_of :name
-  validates_presence_of   :data_fields_types
+  validates_presence_of   :department_id
 end
